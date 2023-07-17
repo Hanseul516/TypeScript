f0012dfa79a67fb8451af59b5b2b7a81c7185c21
최한슬 <hanseulo@naver.com>
2023-07-17 14:12:42 +0900

    타입스크립트 23-07-17

---
+++ build/js/04_Objects.js
@@ -0,0 +1,36 @@
+"use strict";
+// Objects
+let myObj; // object 타입 변수 선언
+myObj = []; // 배열을 할당하여 myObj에 저장
+console.log(typeof myObj); // myObj의 타입 출력 (object)
+myObj = bands; // bands 배열을 할당하여 myObj에 저장
+myObj = {}; // 빈 객체를 할당하여 myObj에 저장
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true
+}; // 객체 리터럴 선언과 초기화
+exampleObj.prop1 = 'John'; // 객체 속성 값 수정
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1988, 5150, 'OU812']
+}; // Guitarist 타입의 객체 선언과 초기화
+let jp = {
+    name: 'Jimmy',
+    active: true,
+    albums: ['I', 'II', 'IV']
+}; // Guitarist 타입의 객체 선언과 초기화
+const greetGuitarist = (guitarist) => {
+    var _a;
+    if (guitarist.name) { //guitarist 객체에 name 속성이 존재하는지 확인
+        return `Hello ${(_a = guitarist.name) === null || _a === void 0 ? void 0 : _a.toUpperCase()}!`; //guitarist 객체에 name 속성이 존재할 경우, 해당 이름을 대문자로 변환한 후, Hello [이름]! 형식의 문자열을 반환. 여기서 ?.는 옵셔널 체이닝 연산자로, guitarist.name이 undefined 또는 null인 경우를 대비하여 안전하게 접근.
+    }
+    return 'Hello!';
+};
+console.log(greetGuitarist(jp)); // jp 객체를 인자로 전달하여 함수 호출
+// - 객체는 여러 개의 속성으로 구성된 자료구조입니다.
+// - 객체는 중괄호 {} 안에 속성과 해당 속성의 값으로 정의됩니다.
+// - 객체의 속성은 특정 타입의 값이 할당될 수 있으며, 속성의 값을 수정할 수 있습니다.
+// - 객체 타입은 속성의 타입과 선택적인 속성을 정의할 수 있습니다.
+// - 객체는 타입을 명시하여 변수를 선언하고, 해당 타입의 객체를 할당하여 사용할 수 있습니다.
+// - 함수의 매개변수로 객체 타입을 사용하여 객체를 전달할 수 있습니다.
---
+++ src/01_Basic.ts
@@ -0,0 +1,27 @@
+// chapter2: Basic Types
+
+let myName: string = 'hanseul';  // 문자열 타입 변수 선언과 초기화
+let meaningOfLife: number;  // 숫자 타입 변수 선언
+let isLoading: boolean;  // 불리언 타입 변수 선언
+let album: any;  // 모든 타입을 저장할 수 있는 변수 선언
+
+myName = 'Risa';  // 변수에 값 할당
+meaningOfLife = 42;
+isLoading = true;
+album = 2000;
+
+const sum = (a: number, b: string) => {  // 숫자와 문자열을 인수로 받는 함수 선언
+  return a + Number(b);  // 인수를 숫자로 변환하여 합산한 값을 반환
+};
+
+let postId: string | number;  // 문자열 또는 숫자 값을 저장할 수 있는 변수 선언
+let isActive: number | boolean | string;  // 숫자, 불리언, 문자열 값을 저장할 수 있는 변수 선언
+
+let re: RegExp = /\w+/g;  // 정규 표현식
+
+
+// - 변수를 선언할 때 타입을 명시하여 해당 변수가 어떤 종류의 값을 가지는지 지정합니다.
+// - 값을 할당할 때는 변수에 해당하는 타입과 일치하는 값을 할당합니다.
+// - 함수의 매개변수와 반환 값에도 타입을 명시하여 함수의 입력과 출력을 명확히합니다.
+// - 유니언 타입을 사용하면 여러 가지 타입 중 하나를 저장할 수 있는 변수를 만들 수 있습니다.
+// - 정규 표현식은 패턴에 대응되는 문자열을 찾을 때 사용됩니다.
