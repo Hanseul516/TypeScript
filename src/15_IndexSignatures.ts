213b14388e0f632296dc4448780c8c3803b5d01b
최한슬 <hanseulo@naver.com>
2023-07-19 11:56:33 +0900

    타입스크립트 23-07-19

--- src/15_IndexSignatures.ts
+++ src/15_IndexSignatures.ts
@@ -1,14 +1,15 @@
 // Index Signatures
 
-interface TransactionObj{
-    
-}
-
 // interface TransactionObj {
-//     Pizza: number,
-//     Books: number,
-//     Job: number
+//     readonly [index: string]: number
 // }
+
+interface TransactionObj {
+    readonly [index: string]: number
+    Pizza: number,
+    Books: number,
+    Job: number
+}
 
 const todaysTransactions: TransactionObj = {
     Pizza: -10,
@@ -17,7 +18,39 @@ const todaysTransactions: TransactionObj = {
 }
 
 console.log(todaysTransactions.Pizza)
-console.log(todaysTransactions.['Pizza'])
+console.log(todaysTransactions['Pizza'])
 
-let prop: string='Pizza'
+let prop: string = 'Pizza'
 console.log(todaysTransactions[prop])
+
+const todaysNet = (transactions: TransactionObj):
+    number => {
+    let total = 0
+    for (const transaction in transactions) {
+        total += transactions[transaction]
+    }
+    return total
+}
+
+console.log(todaysNet(todaysTransactions))
+
+// todaysTransactions.Pizza = 40 //오류
+
+console.log(todaysTransactions['Dave'])
+
+///////////////////////////////////////
+
+interface Student {
+    [Key: string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    Classes?: number[]
+}
+
+const Student: Student = {
+    name: "Doug",
+    GPA: 3.5,
+    Classes: [100, 200]
+}
+
+console.log(Student.test)  //test는 undefined라서 가능
