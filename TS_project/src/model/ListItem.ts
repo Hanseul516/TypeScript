cd515da9cd1f8730769bb3c6b68e69855ccf60b0
최한슬 <hanseulo@naver.com>
2023-07-21 16:20:18 +0900

    미니프로젝트 23-07-21

---
+++ TS_project/src/model/ListItem.ts
@@ -0,0 +1,22 @@
+export interface Item {
+    id: string,
+    item: string,
+    checked: boolean,
+}
+
+export default class ListItem implements Item {
+//class를 만들때 constructor, get, this 세트
+    constructor(
+        private _id: string = '', // = '',는 초기값으로 설정한 거
+        private _item: string = '',
+        private _checked: boolean = false,
+    ){}
+
+    get id(): string{
+        return this._id
+    }
+
+    set id(id:string){
+        this._id = id
+    }
+}
