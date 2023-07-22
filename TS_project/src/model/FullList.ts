f90a423276ee2aab341111c47d543097a66e8845
최한슬 <hanseulo@naver.com>
2023-07-22 11:11:00 +0900

    미니프로젝트 23-07-22

--- TS_project/src/model/FullList.ts
+++ TS_project/src/model/FullList.ts
@@ -2,9 +2,22 @@ import ListItem from "./ListItem";
 
 interface List {
     list: ListItem[],
-    load(): void,
-    save(): void,
+    load(): void, //void: 값을 반환하지 않음
+    save(): void,  //메서드 시그니처(Method Signature)
     clearList(): void,
     addItem(itemObj: ListItem): void,
     removeItem(id: string): void,
+}
+
+export default class FullList implements List {
+
+    static instance: FullList = new FullList()
+
+    private constructor(private _list: ListItem[] = []) { }
+
+    get list(): ListItem[]{
+        return this._list
+    }
+
+    save():
 }
