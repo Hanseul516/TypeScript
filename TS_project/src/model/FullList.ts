e09eee160e42c9124a38ffa890085a367b5a4e54
최한슬 <hanseulo@naver.com>
2023-07-23 16:28:29 +0900

    미니프로젝트 23-07-23

--- TS_project/src/model/FullList.ts
+++ TS_project/src/model/FullList.ts
@@ -15,9 +15,34 @@ export default class FullList implements List {
 
     private constructor(private _list: ListItem[] = []) { }
 
-    get list(): ListItem[]{
+    get list(): ListItem[] {
         return this._list
     }
 
-    save():
+    load(): void {
+        const storedList: string | null = localStorage.getItem("myList") //키의 '값'을 가져옴
+        if (typeof storedList !== "string") return
+
+        const parsedList: {_id: string, _item: string, _checked: boolean}[]
+            =JSON.parse(storedList)
+    }
+
+    save(): void { //setItem: 데이터저장(식별자-데이터)  //stringify: JSON 문자열로 변환
+        localStorage.setItem("myList", JSON.stringify(this._list))
+    }
+
+    clearList(): void {
+        this._list = [] //빈배열로 초기화하고
+        this.save()  //다시 저장
+    }
+
+    addItem(itemObj: ListItem): void {
+        this._list.push(itemObj) //itemObj를 _list 배열에 push
+        this.save() //다시 저장
+    }
+
+    removeItem(id: string): void { //filter: 함수의 반환 값이 true인 새로운 배열생성
+        this._list = this._list.filter(item => item.id !== id) //화살표 함수: (매개변수) => { 함수 본문 }
+        this.save() //다시 저장
+    }
 }
