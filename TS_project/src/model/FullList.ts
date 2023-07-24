47f71d96d74e4717daf4420f2384b132ed72442d
최한슬 <hanseulo@naver.com>
2023-07-24 12:58:41 +0900

    미니프로젝트 23-07-24

--- TS_project/src/model/FullList.ts
+++ TS_project/src/model/FullList.ts
@@ -1,9 +1,9 @@
-import ListItem from "./ListItem";
+import ListItem from './ListItem'
 
 interface List {
     list: ListItem[],
-    load(): void, //void: 값을 반환하지 않음
-    save(): void,  //메서드 시그니처(Method Signature)
+    load(): void,
+    save(): void,
     clearList(): void,
     addItem(itemObj: ListItem): void,
     removeItem(id: string): void,
@@ -20,34 +20,33 @@ export default class FullList implements List {
     }
 
     load(): void {
-        const storedList: string | null = localStorage.getItem("myList") //키의 '값'을 가져옴
+        const storedList: string | null = localStorage.getItem("myList")
         if (typeof storedList !== "string") return
 
-        const parsedList: { _id: string, _item: string, _checked: boolean }[]
-            = JSON.parse(storedList) //parse: JSON 형식 -> JavaScript 객체
+        const parsedList: { _id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)
 
-        parsedList.forEach(itemObj => { //forEach: 지정된 함수를 순서대로 실행
+        parsedList.forEach(itemObj => {
             const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked)
             FullList.instance.addItem(newListItem)
         })
     }
 
-    save(): void { //setItem: 데이터저장(식별자-데이터)  //stringify: JSON 문자열로 변환
+    save(): void {
         localStorage.setItem("myList", JSON.stringify(this._list))
     }
 
     clearList(): void {
-        this._list = [] //빈배열로 초기화하고
-        this.save()  //다시 저장
+        this._list = []
+        this.save()
     }
 
     addItem(itemObj: ListItem): void {
-        this._list.push(itemObj) //itemObj를 _list 배열에 push
-        this.save() //다시 저장
+        this._list.push(itemObj)
+        this.save()
     }
 
-    removeItem(id: string): void { //filter: 함수의 반환 값이 true인 새로운 배열생성
-        this._list = this._list.filter(item => item.id !== id) //화살표 함수: (매개변수) => { 함수 본문 }
-        this.save() //다시 저장
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id)
+        this.save()
     }
 }
