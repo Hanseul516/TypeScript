5488c2a847c55da297d9467f52e23cf55a85d762
최한슬 <hanseulo@naver.com>
2023-07-31 12:48:34 +0900

    투두 리스트

---
+++ TS_project/src/templates/ListTemplate.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ListTemplate = /** @class */ (function () {
+    function ListTemplate() {
+        this.ul = document.getElementById("listItems"); //타입단언
+    }
+    ListTemplate.prototype.clear = function () {
+        this.ul.innerHTML = '';
+    };
+    ListTemplate.prototype.render = function (fullList) {
+        var _this = this;
+        this.clear();
+        fullList.list.forEach(function (item) {
+            var li = document.createElement("li");
+            li.className = "item";
+            var check = document.createElement("input");
+            check.type = "checkbox";
+            check.id = item.id;
+            check.tabIndex = 0;
+            item.checked = item.checked;
+            li.append(check);
+            check.addEventListener('change', function () {
+                item.checked = !item.checked;
+                fullList.save();
+            });
+            var label = document.createElement("label");
+            label.htmlFor = item.id;
+            label.textContent = item.item;
+            li.append(label);
+            var button = document.createElement("button");
+            button.className = 'button';
+            button.textContent = 'X';
+            li.append(button);
+            button.addEventListener('click', function () {
+                fullList.removeItem(item.id);
+                _this.render(fullList); //render: HTML 요소로 변환하여 화면에 보여줌
+            });
+            _this.ul.append(li);
+        });
+    };
+    ListTemplate.instance = new ListTemplate();
+    return ListTemplate;
+}());
+exports.default = ListTemplate;
